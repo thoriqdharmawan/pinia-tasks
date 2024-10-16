eadc286b2eeaea4c16fa1e892cb42177389f11dd
thoriqdharmawan <thoriqd21@gmail.com>
2024-10-17 00:32:25 +0700

    feat(task-detail): add classname condition

--- src/components/TaskDetails.vue
+++ src/components/TaskDetails.vue
@@ -3,7 +3,7 @@
     <h3>{{ task.title }}</h3>
     <div class="icons">
       <i class="material-icons" @click="taskStore.deleteTask(task.id)">delete</i>
-      <i class="material-icons" @click="taskStore.toggleFav(task.id)">favorite</i>
+      <i class="material-icons" :class="{ active: task.isFav }" @click="taskStore.toggleFav(task.id)">favorite</i>
     </div>
   </div>
 </template>
